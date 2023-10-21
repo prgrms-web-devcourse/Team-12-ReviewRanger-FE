06b37c98c9882136380d7ceec88fd9b61b75e1e0
Kim0426 <706shin1728@naver.com>
2023-10-22 00:51:28 +0900

    REV-17 chore: eslint 절대경로 @설정, import규칙 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,77 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', '@types'],
+      },
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'type',
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'unknown',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/pages*',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/hooks*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/components*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/services*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/constants*',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+
+        pathGroupsExcludedImportTypes: ['@tanstack*'],
+        alphabetize: {
+          order: 'asc',
+        },
+      },
     ],
   },
 };
