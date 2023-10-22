1513640102d1957936317ed558302ebee28812a7
Kim0426 <706shin1728@naver.com>
2023-10-22 22:28:35 +0900

    REV-17 chore: 커밋린트 규칙 추가

---
+++ commitlint.config.cjs
@@ -0,0 +1,31 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  plugins: ['commitlint-plugin-function-rules'],
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+
+    'function-rules/scope-enum': [
+      2,
+      'always',
+      ({ header }) => {
+        const regex = /^REV-\d+ (feat|style|fix|refactor|docs|chore)+: .+/;
+        if (regex.test(header)) {
+          return [true];
+        }
+        return [
+          false,
+          '커밋 형식은 REV-(숫자) (feat,style..etc): 메시지의 형태여야 합니다.',
+        ];
+      },
+    ],
+  },
+};
