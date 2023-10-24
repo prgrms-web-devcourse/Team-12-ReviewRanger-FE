b72593bf69c076a9d53b63602b99233f147f4b34
Kim0426 <706shin1728@naver.com>
2023-10-24 18:56:53 +0900

    chore: 초기 세팅 작업 진행 (#4)
    * chore: 초기 세팅 작업 진행 (#3)
    
    * REV-16 chore: 초기 프로젝트 세팅
    
    * REV-17 chore: eslint 절대경로 @설정, import규칙 추가
    
    * REV-17 chore: pages,hooks,components폴더에 import 우선순위 지정
    
    * REV-17 chore: src의 폴더 pages,hooks,,,생성
    
    * REV-17 chore: src/types에서 불필요 코드 제거
    
    * REV-17 chore: 커밋린트 규칙 추가
    
    * REV-17 docs: 이슈 템플릿 추가
    
    * REV-17 chore: axios,tanstack-query,차크라UI 의존성 추가
    
    * REV-17 chore: queryProvider 기본 옵션 수정
    
    * chore: 커밋린트 커밋 규칙 수정
    
    * docs: 커밋
    
    * chore: tailwind설치
    
    * chore: tailwind 적용 안되는 부분 수정
    
    ---------
    
    Co-authored-by: Kim0426 <706shin1728@naver.com>
    
    * chore: prettier 설정
    
    * chore: 허스키 스크립트 부분 추가
    
    * chore: 개발서버 배포 설정
    
    * chore: 개발서버 열기 전 스크립트 추가
    
    * chore: 불필요 코드 제거
    
    * chore: 개발서버 자동 배포 yml 수정
    
    * chore: return전 빈 줄 추가하는 규칙 설정
    
    * chore: 린트 규칙 수정사항 반영
    
    ---------
    
    Co-authored-by: hyoribogo <97094709+hyoribogo@users.noreply.github.com>

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <ChakraProvider>
+      <h1 className="bg-blue-500 text-red-500">Hellosadasds</h1>
+    </ChakraProvider>
+  )
+}
+
+export default App
---
+++ src/apis/apiClient.ts
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: '',
+})
+
+export default apiClient
