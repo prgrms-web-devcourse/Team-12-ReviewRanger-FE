9bbaf1714ad29a22a509a5dae737a0d7ffd7e096
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-10-29 18:50:40 +0900

    chore: commitlint 규칙 변경

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,17 +2,7 @@
 module.exports = {
   plugins: ['commitlint-plugin-function-rules'],
   rules: {
-    'body-leading-blank': [1, 'always'],
-    'body-max-line-length': [2, 'always', 100],
-    'footer-leading-blank': [1, 'always'],
-    'footer-max-line-length': [2, 'always', 100],
     'header-max-length': [2, 'always', 100],
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-    ],
-
     'function-rules/scope-enum': [
       2,
       'always',
@@ -24,7 +14,7 @@ module.exports = {
 
         return [
           false,
-          '커밋 형식은 (feat,style..etc): 메시지의 형태여야 합니다.',
+          '커밋 형식은 "prefix: commit message"의 형태여야 합니다.',
         ]
       },
     ],
