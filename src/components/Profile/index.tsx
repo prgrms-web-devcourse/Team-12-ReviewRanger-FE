3c915e8dff7fd3cb2ee05fc416c72ff07f4c0217
hayamaster <dnfxmfkdbzos@naver.com>
2023-10-30 13:01:59 +0900

    style: 이미지 크기 수정

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -10,7 +10,7 @@ const Profile = ({ image = <BasicProfileIcon />, name }: ProfileProps) => {
   return (
     <>
       <div className="flex flex-row items-center gap-2">
-        <div className="flex h-[35px] w-[35px]">
+        <div className="flex h-[30px] w-[30px]">
           {typeof image === 'string' ? (
             <img src={image} className="object-cover" />
           ) : (
