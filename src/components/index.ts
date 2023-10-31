5f022cc53f317a684df59b7afd6d63354eacd0d1
Kim0426 <706shin1728@naver.com>
2023-10-31 15:41:01 +0900

    feat: 비밀번호 입력 컴포넌트 구현 (#13)
    * feat: 비밀번호 입력 컴포넌트 구현
    
    * refactor: input 타입 변경
    
    * refactor: 함수명 수정,event->e로 네이밍 변경
    
    * refactor: 수정 사항 반영

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as PassWordInput } from './PasswordInput'
 export { default as StarRating } from './StarRating'
 export { default as IconButton } from './IconButton'
 export { default as SearchBar } from './SearchBar'
