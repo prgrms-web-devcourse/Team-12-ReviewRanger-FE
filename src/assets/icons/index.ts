fff7793b9417badb1262c0c7a2891cb576b38cf2
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-10-31 16:05:54 +0900

    feat: 질문 형식 컴포넌트 구현 (#12)
    * chore: tailwind rippleUI 다크모드 테마 제거
    
    * feat: 질문 형식 컴포넌트 구현
    
    * refactor: 질문 id prop 제거

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as CloseIcon } from './close.svg?react'
+export { default as MenuIcon } from './menu.svg?react'
 export { default as BasicProfileIcon } from './basicProfile.svg?react'
 export { default as PassWord } from './password.svg?react'
 export { default as PassWordConfirm } from './passwordconfirm.svg?react'
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as QuestionItem } from './QuestionItem'
 export { default as PageIntro } from './PageIntro'
 export { default as Profile } from './Profile'
 export { default as UserList } from './UserList'
