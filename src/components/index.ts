fd7c287c8bd84863cee03317c2ee06f6e8037527
Kim0426 <706shin1728@naver.com>
2023-10-31 15:35:39 +0900

    feat: 별점 컴포넌트 구현 (#11)
    * chore: 랜덤한 key id를 위한 uuid 설치
    
    * feat: 별점 컴포넌트를 위한 커스텀 훅 추가
    
    * feat: 평점 각각에 대한 아이템 컴포넌트 개발
    
    * feat: index.ts파일 추가
    
    * refactor: 이상한 함수명 수정
    
    * refactor: 수정사항 반영
    
    * chore: uuid 라이브러리 제거
    
    ---------
    
    Co-authored-by: hyoribogo <97094709+hyoribogo@users.noreply.github.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
-export * from './IconButton'
-
+export { default as StarRating } from './StarRating'
+export { default as IconButton } from './IconButton'
 export { default as SearchBar } from './SearchBar'
 export { default as Profile } from './Profile'
-
