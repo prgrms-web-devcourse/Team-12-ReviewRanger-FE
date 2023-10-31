1ad72d2cc1677e358db5eba68a99918b92e1ab31
JUHA KIM <88622675+hayamaster@users.noreply.github.com>
2023-10-31 15:12:33 +0900

    feat: 검색 input 컴포넌트 구현 (#7)
    * feat: SearchIcon 추가
    
    * feat: SearchBar 컴포넌트 기능 구현
    
    ---------
    
    Co-authored-by: Kim0426 <706shin1728@naver.com>

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as CloseIcon } from './close.svg?react'
+export { default as SearchIcon } from './search.svg?react'
 export { default as BasicProfileIcon } from './basicProfile.svg?react'
+
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
+export { default as SearchBar } from './SearchBar'
 export { default as Profile } from './Profile'
+
