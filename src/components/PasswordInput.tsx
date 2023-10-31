4ffe0f9313814d6d20e30901d593cd0fe0054db4
Kim0426 <706shin1728@naver.com>
2023-10-31 12:46:31 +0900

    refactor: 함수명 수정,event->e로 네이밍 변경

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -4,7 +4,7 @@ import { PassWord, PassWordConfirm } from '@/assets/icons'
 interface PasswordInputProps
   extends Omit<ComponentPropsWithoutRef<'input'>, 'onChange'> {
   type: 'password' | 'confirmation'
-  onChange?: (newPassword?: string) => void
+  handlePassWordChange?: (newPassword?: string) => void
   disabled?: boolean
   placeholder?: string
 }
@@ -13,21 +13,19 @@ const PassWordInput = ({
   type,
   placeholder,
   disabled,
-  onChange,
-  ...rest // 나머지 프로퍼티를 수집
+  handlePassWordChange,
+  ...rest
 }: PasswordInputProps) => {
-  const handlePassWordChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange && onChange(event.target.value)
-  }
-
   return (
     <div className="items-center2 relative flex h-fit w-fit flex-row content-center justify-center gap-2 border-2 border-black">
       <input
         className="input border-0 bg-white pl-8 text-black"
         placeholder={placeholder ?? '새 비밀번호 입력'}
         disabled={disabled}
-        onChange={handlePassWordChange}
-        {...rest} // 나머지 프로퍼티를 전달
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          handlePassWordChange && handlePassWordChange(e.target.value)
+        }
+        {...rest}
       />
       <i className="flex items-center">
         {type === 'password' ? <PassWord /> : <PassWordConfirm />}
