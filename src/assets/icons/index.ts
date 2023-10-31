8bd8004eea064a5ae94a967f77db19e00a18d870
Kim0426 <706shin1728@naver.com>
2023-10-31 15:23:35 +0900

    feat: 아이콘 버튼 컴포넌트 구현 (#8)
    * feat: 기본 버튼 컴포넌트 추가
    
    * feat: 아이콘 컴포넌트 children으로 설정
    
    * feat: index.ts폴더 추가
    
    * refactor: 기본 변수 수정
    
    * refactor: 추가적인 클래스 받도록 설정
    
    * refactor: className추가
    
    * refactor: 불필요한 prop,유틸 제거
    
    * refactor: 수정사항 반영

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as CloseIcon } from './close.svg?react'
+export { default as TableSend } from './table_send.svg?react'
 export { default as SearchIcon } from './search.svg?react'
 export { default as BasicProfileIcon } from './basicProfile.svg?react'
 
