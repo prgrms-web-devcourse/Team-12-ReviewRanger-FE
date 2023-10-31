0be7cff128efa3b0fb1ebfde19e22292b8dee02c
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-10-31 16:04:10 +0900

    refactor: 수정 사항 반영

--- src/components/PageIntro/index.tsx
+++ src/components/PageIntro/index.tsx
@@ -1,25 +1,26 @@
 interface PageIntroProps {
   imageSrc?: string
   description: string
-  colorTheme: string
+  className?: string
 }
 
-const PageIntro = ({ imageSrc, description, colorTheme }: PageIntroProps) => {
-  const boxDefaultStyle = `border-2 p-4 whitespace-pre-line border-${colorTheme}-500`
-  const textDefaultStyle = `text-${colorTheme}-900 text-sm sm:text-base`
-
-  return imageSrc ? (
-    <div className={`flex items-center gap-x-8 ${boxDefaultStyle}`}>
-      <img
-        className="max-w-20 max-h-20 object-cover"
-        src={imageSrc}
-        alt="페이지를 소개하는 npc 아이콘"
-      />
-      <p className={`${textDefaultStyle}`}>{description}</p>
-    </div>
-  ) : (
-    <div className={`${boxDefaultStyle}`}>
-      <p className={`${textDefaultStyle}`}>{description}</p>
+const PageIntro = ({
+  imageSrc,
+  description,
+  className = '',
+}: PageIntroProps) => {
+  return (
+    <div
+      className={`flex items-center gap-x-8 whitespace-pre-line border-2 p-4 ${className}`}
+    >
+      {imageSrc && (
+        <img
+          className="max-w-20 max-h-20 object-cover"
+          src={imageSrc}
+          alt="페이지를 소개하는 npc 아이콘"
+        />
+      )}
+      <p className={`text-sm sm:text-base`}>{description}</p>
     </div>
   )
 }
