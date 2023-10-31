313d2f6ae273d5e39cf6da194e982e846525c98c
Kim0426 <706shin1728@naver.com>
2023-10-31 15:21:36 +0900

    refactor: 수정사항 반영

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -2,56 +2,22 @@ import { PropsWithChildren } from 'react'
 import './IconButton.css'
 
 interface IconButtonProps {
-  color?:
-    | 'red'
-    | 'blue'
-    | 'green'
-    | 'yellow'
-    | 'pink'
-    | 'purple'
-    | 'gray'
-    | 'slate'
-    | 'primary'
-    | 'secondary'
-    | 'error'
-    | 'success'
-    | 'warning'
-  background?:
-    | 'red'
-    | 'blue'
-    | 'green'
-    | 'yellow'
-    | 'pink'
-    | 'purple'
-    | 'gray'
-    | 'slate'
-    | 'primary'
-    | 'secondary'
-    | 'error'
-    | 'success'
-    | 'warning'
-  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
   disabled?: boolean
   className?: string
   text?: string
 }
 
 const IconButton = ({
-  color,
-  background,
-  size,
   disabled,
   className,
   children,
   text,
 }: PropsWithChildren<IconButtonProps>) => {
-  const iconBtnClasses = `iconbtn iconbtn-color--${color} iconbtn-background--${background} iconbtn-${size} ${
-    disabled ? 'iconbtn-disabled' : ''
-  }`
-
   return (
     <button
-      className={`flex h-14 w-36 items-center bg-white text-black ${iconBtnClasses} ${className} `}
+      className={`btn flex h-14 w-36 items-center gap-2 border border-black bg-white text-black  ${
+        disabled ? 'btn-disabled' : ''
+      } ${className} `}
     >
       {children}
       {text ?? 'Default'}
