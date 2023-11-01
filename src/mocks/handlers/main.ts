324e282997c8632d33d0d67a0b4990be4a9c44c8
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-01 20:29:01 +0900

    feat: 응답 수정 뮤테이션 hook 구현

--- src/mocks/handlers/main.ts
+++ src/mocks/handlers/main.ts
@@ -23,4 +23,7 @@ export const mainHandlers = [
   rest.get('/received-reviews/:afterResultId', async (_, res, ctx) => {
     return res(ctx.status(200), ctx.json(REVIEW_RESULT))
   }),
+  rest.put('/invited-surveys', async (_, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ success: true }))
+  }),
 ]
