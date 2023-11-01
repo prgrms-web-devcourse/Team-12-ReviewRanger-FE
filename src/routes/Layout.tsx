98726f0180bcb93a64ef4b92a4270e32da60b031
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-01 15:41:17 +0900

    style: 레이아웃 다크모드 색 적용

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div className="mx-auto h-screen max-w-[767px] border border-black md:max-w-[880px]">
+    <div className="mx-auto h-screen max-w-[767px] border border-black dark:border-white dark:bg-[#202020] md:max-w-[880px]">
       <Outlet />
     </div>
   )
