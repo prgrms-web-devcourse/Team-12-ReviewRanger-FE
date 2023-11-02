2c32248630b359f38fe1faa80da152a3d8e8bb8c
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-02 11:01:25 +0900

    feat: api hook 사용하기 위한 로그인 페이지 간단 기능 구현

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,5 +1,49 @@
-//로그인 페이지
+import { useState, ChangeEvent } from 'react'
+import { useCreateLogin } from '@/apis/hooks'
+
 const LoginPage = () => {
-  return <>로그인 페이지</>
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const { mutate: loginMutate, data: loginToken } = useCreateLogin()
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+
+  const handleLoginButtonClick = () => {
+    loginMutate({ email, password })
+  }
+
+  return (
+    <div className="flex w-fit flex-col gap-2">
+      <div>로그인 페이지</div>
+      <input
+        value={email}
+        type="text"
+        className="border border-black"
+        onChange={handleEmailChange}
+        placeholder="email"
+      />
+      <input
+        value={password}
+        type="text"
+        className="border border-black"
+        onChange={handlePasswordChange}
+        placeholder="password"
+      />
+      <button className="btn" onClick={handleLoginButtonClick}>
+        로그인
+      </button>
+      <div className="flex flex-row gap-2">
+        <p className="font-bold">JWT : </p>
+        <p>{loginToken?.data}</p>
+      </div>
+    </div>
+  )
 }
 export default LoginPage
