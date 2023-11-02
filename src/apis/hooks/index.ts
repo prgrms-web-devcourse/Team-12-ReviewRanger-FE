a576b8fb751b2b0db4a71b4d0badaa90f115b93a
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-02 09:07:08 +0900

    feat: useCreateCheckDuplicatedEmail hook 구현

--- src/apis/hooks/index.ts
+++ src/apis/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useCreateSignUp } from './useCreateSignUp'
 export { default as useCreateCheckDuplicatedName } from './useCreateCheckDuplicatedName'
+export { default as useCreateCheckDuplicatedEmail } from './useCreateCheckDuplicatedEmail'
