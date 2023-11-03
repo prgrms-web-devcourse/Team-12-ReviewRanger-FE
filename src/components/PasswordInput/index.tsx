c1b187f5af6b1a7d872e0bf520a9537361b679df
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-04 03:10:43 +0900

    refactor: passwordInput 컴포넌트 수정

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -1,18 +1,24 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FocusEvent, RefObject, useState } from 'react'
 import { EyeOffIcon, EyeOnIcon } from '@/assets/icons'
 
 interface PasswordInputProps {
+  passwordRef?: RefObject<HTMLInputElement>
   handlePasswordChange?: (e: ChangeEvent<HTMLInputElement>) => void
+  handlePasswordBlur?: (e: FocusEvent<HTMLInputElement>) => void
   disabled?: boolean
   type?: 'password' | 'passwordConfirm'
   placeholder?: string
+  passwordMsg?: string
 }
 
 const PasswordInput = ({
+  passwordRef,
   placeholder = '비밀번호를 입력하세요.',
   disabled,
   type,
   handlePasswordChange,
+  handlePasswordBlur,
+  passwordMsg,
   ...rest
 }: PasswordInputProps) => {
   const [showPassword, setShowPassword] = useState(false)
@@ -23,15 +29,22 @@ const PasswordInput = ({
 
   return (
     <div className="flex flex-col justify-center gap-[0.44rem] border-2 border-black px-[0.63rem] pb-[0.69rem] pt-[0.31rem]">
-      <div className="h-4 text-[0.625rem] text-[#BABABA]">
-        {type === 'password' ? '비밀번호' : '비밀번호 확인'}
+      <div className="flex flex-row justify-between">
+        <div className="h-4 text-[0.625rem] text-[#BABABA]">
+          {type === 'password' ? '비밀번호' : '비밀번호 확인'}
+        </div>
+        {passwordMsg && (
+          <div className="text-[0.625rem] text-red-400">{passwordMsg}</div>
+        )}
       </div>
       <div className="flex flex-row">
         <input
+          ref={passwordRef}
           className="h-4 flex-1 border-0 text-xs text-black focus:outline-none"
           placeholder={placeholder}
           disabled={disabled}
           onChange={handlePasswordChange}
+          onBlur={handlePasswordBlur}
           type={showPassword ? 'text' : 'password'}
           {...rest}
         />
