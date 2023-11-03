3aa379a1e32c8f6a008288589ddb155fea6533ce
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-04 03:12:02 +0900

    refactor: usePasswordCheck hook 수정

--- src/pages/SignUpPage/hooks/usePasswordCheck.ts
+++ src/pages/SignUpPage/hooks/usePasswordCheck.ts
@@ -1,14 +1,30 @@
-import { useState, ChangeEvent } from 'react'
+import { useState, ChangeEvent, Dispatch, SetStateAction, useRef } from 'react'
+import { checkPasswordPattern } from '@/utils'
 
-const usePasswordCheck = () => {
+interface UsePasswordCheckProps {
+  setPasswordFailMsg: Dispatch<SetStateAction<string>>
+}
+
+const usePasswordCheck = ({ setPasswordFailMsg }: UsePasswordCheckProps) => {
+  const passwordRef = useRef(null)
   const [password, setPassword] = useState('')
+
+  const handlePasswordFocusChange = () => {
+    if (passwordRef.current !== document.activeElement) {
+      const msg = checkPasswordPattern({ password })
+      setPasswordFailMsg(msg)
+    }
+  }
+
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value)
+    setPassword(e.target.value.trim())
   }
 
   return {
     password,
+    passwordRef,
     handlePasswordChange,
+    handlePasswordFocusChange,
   }
 }
 
