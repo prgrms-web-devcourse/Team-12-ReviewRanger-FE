b26847e72db7cb6cc0c7e20e4ecd3790ce50bfe1
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-04 02:30:42 +0900

    feat: 이름 유효성 검사 util함수 추가

--- src/utils/authValidation.ts
+++ src/utils/authValidation.ts
@@ -1,7 +1,15 @@
 const EMAIL_REGEXP = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/
+// const NAME_REGEXP = /[~!@#$%^&*()_+|<>?:{}]/
+const NAME_REGEXP = /^[a-zA-Z0-9가-힣]+$/
+const NAME_MIN_LENGTH = 3
+const NAME_MAX_LENGTH = 10
 
 interface CheckEmailPatternProps {
   email: string
+}
+
+interface CheckNamePatternProps {
+  name: string
 }
 
 export const checkEmailPattern = ({ email }: CheckEmailPatternProps) => {
@@ -9,6 +17,20 @@ export const checkEmailPattern = ({ email }: CheckEmailPatternProps) => {
 
   if (!EMAIL_REGEXP.test(trimmedEmail)) {
     return '유효한 이메일 주소가 아니야!'
+  } else {
+    return ''
+  }
+}
+
+export const checkNamePattern = ({ name }: CheckNamePatternProps) => {
+  const trimmedName = name.trim()
+
+  if (
+    !NAME_REGEXP.test(trimmedName) ||
+    trimmedName.length < NAME_MIN_LENGTH ||
+    trimmedName.length > NAME_MAX_LENGTH
+  ) {
+    return '3 ~ 10자의 문자 또는 숫자로 만들라구!'
   } else {
     return ''
   }
