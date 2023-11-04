005cc9ca07a0b107afefc3598d988b45f6d80fb8
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-04 23:39:49 +0900

    feat: 개발용 다크모드 토글 버튼 추가

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -2,7 +2,15 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div className="mx-auto h-screen max-w-[767px] border border-black dark:border-white dark:bg-[#202020] md:max-w-[880px]">
+    <div className="border-x-1 mx-auto min-h-screen max-w-[1000px] border-black bg-main-yellow dark:border-white dark:bg-main-red-100">
+      <button
+        className="btn fixed right-40 top-4 bg-pink-800 text-white"
+        onClick={() => {
+          document.documentElement.classList.toggle('dark')
+        }}
+      >
+        토글
+      </button>
       <Outlet />
     </div>
   )
