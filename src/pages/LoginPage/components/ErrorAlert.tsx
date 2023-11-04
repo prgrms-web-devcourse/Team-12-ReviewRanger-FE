5b1d81280625aff427dbb913994e7a649bebb88e
Kim0426 <706shin1728@naver.com>
2023-11-04 23:35:00 +0900

    refactor: ErrorAlrt 최대 길이 수정

--- src/pages/LoginPage/components/ErrorAlert.tsx
+++ src/pages/LoginPage/components/ErrorAlert.tsx
@@ -5,23 +5,11 @@ interface ErrorAlertProps {
 const ErrorAlert = ({ errortext }: ErrorAlertProps) => {
   return (
     <div className="alert alert-error max-w-sm">
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 48 48"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M24 4C12.96 4 4 12.96 4 24C4 35.04 12.96 44 24 44C35.04 44 44 35.04 44 24C44 12.96 35.04 4 24 4ZM24 26C22.9 26 22 25.1 22 24V16C22 14.9 22.9 14 24 14C25.1 14 26 14.9 26 16V24C26 25.1 25.1 26 24 26ZM26 34H22V30H26V34Z"
-          fill="#E92C2C"
-        />
-      </svg>
       <div className="flex w-full justify-between">
         <div className="flex flex-col">
-          <div className="text-content2 text-sub-red-200">{errortext}</div>
+          <div className="flex max-w-[300px] flex-wrap text-content2 text-sub-red-200">
+            {errortext}
+          </div>
         </div>
       </div>
     </div>
