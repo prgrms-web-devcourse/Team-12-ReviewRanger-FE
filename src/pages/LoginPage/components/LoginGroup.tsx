9a4605a2b353bc5ae1d3705ab9d37afad12c18bf
Kim0426 <706shin1728@naver.com>
2023-11-04 16:45:14 +0900

    refactor: 로그인 그룹 마크업 수정

--- src/pages/LoginPage/components/LoginGroup.tsx
+++ src/pages/LoginPage/components/LoginGroup.tsx
@@ -14,7 +14,6 @@ const LoginGroup = forwardRef(
     const passWordRef = useRef<HTMLInputElement | null>(null)
 
     const [validEmail, setValidEmail] = useState<boolean>(true)
-
     const handleFocusEmail = () => {
       setValidEmail(true)
     }
@@ -36,24 +35,30 @@ const LoginGroup = forwardRef(
 
     return (
       <div className="bg-ivory flex h-full flex-col items-center justify-center gap-4 dark:bg-main-red-100">
-        <input
-          className="rounded-5 w-[350px] max-w-[350px] border-2 border-black dark:h-[60px] dark:border-white dark:bg-main-red-100 dark:text-white"
-          ref={emailRef}
-          type="text"
-          placeholder="email"
-          onBlur={handleBlurEmail}
-          onFocus={handleFocusEmail}
-        />
+        <div className="flex h-[60px] w-[350px] max-w-[350px] flex-col border-2  border-black dark:border-white dark:text-white">
+          <label className="text-gray-100">이메일</label>
+          <input
+            className="rounded-5 border-0  outline-0 dark:border-white dark:bg-main-red-100 dark:text-white"
+            ref={emailRef}
+            type="text"
+            placeholder="email"
+            onBlur={handleBlurEmail}
+            onFocus={handleFocusEmail}
+          />
+        </div>
         {!validEmail && (
           <ErrorAlert errortext="올바른 이메일 형식이 아닙니다" />
         )}
+        <div className="flex h-[60px] w-[350px] max-w-[350px] flex-col border-2  border-black dark:border-white dark:text-white">
+          <label className="text-gray-100">비밀번호</label>
+          <input
+            className="rounded-5 border-0  outline-0 dark:border-white dark:bg-main-red-100 dark:text-white"
+            ref={passWordRef}
+            type="password"
+            placeholder="password"
+          />
+        </div>
 
-        <input
-          className="rounded-5 w-[350px] max-w-[350px] border-2 border-black dark:h-[60px] dark:border-white dark:bg-main-red-100 dark:text-white"
-          ref={passWordRef}
-          type="password"
-          placeholder="password"
-        />
         <button
           className="rounded-5 btn h-[54px]  w-[350px] max-w-[350px] dark:bg-active-orange "
           onClick={props.handleLogin}
