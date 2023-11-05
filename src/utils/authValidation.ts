8dc4601b9bbcbde2e1f8aab86a9a257719c27394
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-06 04:05:10 +0900

    refactor: [리뷰 반영] authValidation 파일 상수 처리

--- src/utils/authValidation.ts
+++ src/utils/authValidation.ts
@@ -3,6 +3,11 @@ const PASSWORD_REGEXP = /^(?=.*[a-zA-Z])(?=.*[0-9]).{5,15}$/
 const NAME_REGEXP = /^[a-zA-Z0-9가-힣[\]-]+$/
 const NAME_MIN_LENGTH = 3
 const NAME_MAX_LENGTH = 12
+const VALID_MESSAGE = ''
+const INVALID_EMAIL_MESSAGE = '유효한 이메일 주소가 아니야!'
+const INVALID_NAME_MESSAGE = '3 ~ 12자의 문자 또는 숫자로만 만들라구!'
+const INVALID_PASSWORD_MESSAGE = '5 ~ 15자로 문자와 숫자를 포함해야 만들라구!'
+const INVALID_PASSWORD_CONFIRM_MESSAGE = '비밀번호가 일치하지 않는군!'
 
 interface CheckEmailPatternProps {
   email: string
@@ -25,9 +30,9 @@ export const checkEmailPattern = ({ email }: CheckEmailPatternProps) => {
   const trimmedEmail = email.trim()
 
   if (!EMAIL_REGEXP.test(trimmedEmail)) {
-    return '유효한 이메일 주소가 아니야!'
+    return INVALID_EMAIL_MESSAGE
   } else {
-    return ''
+    return VALID_MESSAGE
   }
 }
 
@@ -39,9 +44,9 @@ export const checkNamePattern = ({ name }: CheckNamePatternProps) => {
     trimmedName.length < NAME_MIN_LENGTH ||
     trimmedName.length > NAME_MAX_LENGTH
   ) {
-    return '3 ~ 12자의 문자 또는 숫자로만 만들라구!'
+    return INVALID_NAME_MESSAGE
   } else {
-    return ''
+    return VALID_MESSAGE
   }
 }
 
@@ -51,9 +56,9 @@ export const checkPasswordPattern = ({
   const trimmedPassword = password.trim()
 
   if (!PASSWORD_REGEXP.test(trimmedPassword)) {
-    return '5 ~ 15자로 문자와 숫자를 포함해야 만들라구!'
+    return INVALID_PASSWORD_MESSAGE
   } else {
-    return ''
+    return VALID_MESSAGE
   }
 }
 
@@ -65,8 +70,8 @@ export const checkPasswordConfirmPattern = ({
   const trimmedPasswordConfirm = passwordConfirm.trim()
 
   if (trimmedPassword !== trimmedPasswordConfirm) {
-    return '비밀번호가 일치하지 않는군!'
+    return INVALID_PASSWORD_CONFIRM_MESSAGE
   } else {
-    return ''
+    return VALID_MESSAGE
   }
 }
