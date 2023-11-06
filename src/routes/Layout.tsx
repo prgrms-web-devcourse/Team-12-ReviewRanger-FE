7e9b3835e9fc1e7db284f0e0e6a096d0da06bbce
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-06 13:43:36 +0900

    style: 다크모드 토글 버튼 위치 변경

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -2,17 +2,19 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div className="border-x-1 mx-auto min-h-screen max-w-[1000px] border-black bg-main-yellow dark:border-white dark:bg-main-red-100">
+    <>
+      <div className="border-x-1 mx-auto min-h-screen max-w-[1000px] border-black bg-main-yellow dark:border-white dark:bg-main-red-100">
+        <Outlet />
+      </div>
       <button
-        className="btn fixed right-40 top-4 bg-pink-800 text-white"
+        className="btn fixed bottom-10  right-20 bg-pink-800 text-white"
         onClick={() => {
           document.documentElement.classList.toggle('dark')
         }}
       >
         토글
       </button>
-      <Outlet />
-    </div>
+    </>
   )
 }
 
