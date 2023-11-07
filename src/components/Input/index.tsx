90762ccef52f8305b3069be1420773ab0f08a21d
JUHA KIM <88622675+hayamaster@users.noreply.github.com>
2023-11-07 13:31:48 +0900

    feat: 회원가입 페이지 구현 (#30)
    * refactor: useEmailCheck 훅 만들어 비즈니스 로직 은닉
    
    * refactor: useNameCheck 훅 만들어 비즈니스 로직 은닉
    
    * refactor: usePasswordCheck 훅 만들어 비즈니스 로직 은닉
    
    * refactor: hooks/index.ts 생성
    
    * refactor: useSignUpCheck 훅 만들어 비즈니스 로직 은닉
    
    * refactor: eye-on, eye-off 아이콘 변경
    
    * refactor: useEditProfile put에서 patch로 변경
    
    * refactor: useEmailCheck hook 수정
    
    * feat: 이메일 유효성 검사 util함수 추가
    
    * feat: 이름 유효성 검사 util함수 추가
    
    * refactor: useNameCheck hook 수정
    
    * style: passwordInput 컴포넌트 디자인 수정
    
    * refactor: passwordInput 컴포넌트 수정
    
    * feat: 비밀번호 유효성 검사 util함수 추가
    
    * refactor: usePasswordCheck hook 수정
    
    * refactor: passwordInput 컴포넌트 prop 이름 수정
    
    * feat: usePasswordConfirmCheck hook 추가
    
    * feat: 비밀번호 확인 유효성 검사 util 함수 추가
    
    * refactor: usePasswordConfirmCheck 파일 삭제
    
    * refactor: 비밀번호 확인 부분 함께 추가
    
    * refactor: useSignUpCheck 수정
    
    * refactor: passwordInput 스타일 전역 상수로 변경
    
    * refactor: passwordInput placeholder 기본값 변경
    
    * refactor: passwordInput input 폰트 사이즈 변경
    
    * refactor: EmailInput 공통 컴포넌트 구현
    
    * refactor: passwordInput컴포넌트 삭제
    
    * refactor: emailInput 컴포넌트 삭제
    
    * feat: Input 공통 컴포넌트 구현
    
    * refactor: Input 컴포넌트 적용
    
    * feat: 리뷰레인저 로고 및 이름 아이콘 추가
    
    * refactor: 회원가입 버튼 활성화 조건 수정
    
    * style: Input 컴포넌트 스타일 수정
    
    * style: SignUpPage 스타일 추가
    
    * feat: 회원가입 성공 시, 로그인 페이지 이동 기능 구현
    
    * style: SignUpPage 다크모드 적용
    
    * refactor: rangersIcon svg 파일 기본 사이즈 지정
    
    * style: SignUpPage 웹 반응형 스타일 적용
    
    * refactor: 이름 유효성 정규식 수정
    
    * refactor: 이메일 유효성 검사 수정
    
    * style: eye svg파일 다크 모드 fill 색상 수정
    
    * refactor: useSignUpCheck 콜백 지옥 해결
    
    * style: text 반응형 수정
    
    * refactor: logos 폴더 삭제
    
    * refactor: images 폴더 생성 후, png 및 logo 생성
    
    * refactor: [리뷰 반영] 모든 msg -> message로 변경
    
    * feat: input에 focus 해제될때마다 동작하는 유효성 검사를 매  타자 칠때마다 검사로 수정
    
    * refactor: [리뷰 반영] authValidation 파일 상수 처리
    
    * refactor: AUTH_VALIDATION 상수 만들기
    
    * refactor: DUPLICATED_MESSAGE 상수 처리
    
    * refactor: [리뷰 반영] InputProps 타입을 확장성을 고려하여 수정
    
    * style: light 모드일때 비밀번호 eye 아이콘 색상 지정
    
    * refactor: 비밀번호 & 이름 유효성 검사 수정
    
    * refactor: 이메일과 이름 중복검사를 promise.all로 처리
    
    * refactor: input 컴포넌트에서 prop의 rest 삭제

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,62 @@
+import { ChangeEvent, ComponentPropsWithoutRef, useState } from 'react'
+import { EyeOffIcon, EyeOnIcon } from '@/assets/icons'
+import { INPUT_TYPE } from './constants'
+
+interface InputProps
+  extends Omit<ComponentPropsWithoutRef<'input'>, 'disabled'> {
+  handleInputChange?: (e: ChangeEvent<HTMLInputElement>) => void
+  disabled?: boolean
+  type: keyof typeof INPUT_TYPE
+  message?: string
+}
+
+const Input = ({ disabled, type, handleInputChange, message }: InputProps) => {
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleEyeClick = () => {
+    setShowPassword((prev) => !prev)
+  }
+
+  return (
+    <div className="flex flex-col justify-center gap-[0.44rem] rounded-md border border-gray-100 bg-white px-[0.63rem] pb-[0.69rem] pt-[0.31rem] focus-within:border-black dark:bg-main-red-200 dark:focus-within:border-white">
+      <div className="flex flex-row justify-between">
+        <div className="h-4 text-xs text-gray-100 md:text-sm">
+          {INPUT_TYPE[type].TITLE}
+        </div>
+        {message && (
+          <div className="text-xs text-sub-red-200 dark:text-sub-yellow md:text-sm">
+            {message}
+          </div>
+        )}
+      </div>
+      <div className="flex flex-row">
+        <input
+          className="h-4 flex-1 border-0 bg-white text-sm text-black focus:outline-none dark:bg-main-red-200 dark:text-white md:text-lg"
+          placeholder={INPUT_TYPE[type].PLACEHOLDER}
+          disabled={disabled}
+          onChange={handleInputChange}
+          type={
+            type.includes('password') && !showPassword ? 'password' : 'text'
+          }
+        />
+        {type.includes('password') && (
+          <i className="mx-2 w-fit cursor-pointer">
+            {showPassword ? (
+              <EyeOnIcon
+                onClick={handleEyeClick}
+                className="fill-black dark:fill-white"
+              />
+            ) : (
+              <EyeOffIcon
+                onClick={handleEyeClick}
+                className="fill-black dark:fill-white"
+              />
+            )}
+          </i>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Input
---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+export const AUTH_VALIDATION = {
+  MESSAGE: {
+    VALID: '',
+    INVALID: {
+      EMAIL: '유효한 이메일 주소가 아니야!',
+      NAME: '2 ~ 12자의 문자 또는 숫자로만 만들라구!',
+      PASSWORD: '6 ~ 16자로 문자와 숫자를 포함해야 만들라구!',
+      PASSWORD_CONFIRM: '비밀번호가 일치하지 않는군!',
+    },
+  },
+  EMAIL_REGEXP: /[a-z0-9]+@[a-z]+\.[a-z]{2,8}$/,
+  PASSWORD_REGEXP: /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,16}$/,
+  NAME_REGEXP: /^[a-zA-Z0-9가-힣[\]-]+$/,
+  NAME_MIN_LENGTH: 2,
+  NAME_MAX_LENGTH: 12,
+}
