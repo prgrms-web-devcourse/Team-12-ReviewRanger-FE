bb31e34456c708fd0d9d60df766052c10cb66e1c
Kim0426 <706shin1728@naver.com>
2023-11-07 18:21:26 +0900

    feat: 생성한 리뷰 단일 페이지 컴포넌트 export문 추가

---
+++ src/pages/CreatedReviewManagePage/components/index.ts
@@ -0,0 +1,4 @@
+export { default as ReviewManageTabs } from './ReviewManageTabs'
+export { default as ReviewSelectedUser } from './ReviewSelectedUser'
+export { default as AllResponseReviewByResponser } from './AllResponseReviewByResponser'
+export { default as AllResponseReviewByReceiver } from './AllResponseReviewByReceiver'
--- src/pages/CreatedReviewManagePage/index.tsx
+++ src/pages/CreatedReviewManagePage/index.tsx
@@ -1,25 +1,49 @@
 //생성한 리뷰 관리 페이지
-import { useSendReview } from '@/apis/hooks'
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { useGetAllResponse } from '@/apis/hooks'
+import {
+  ReviewManageTabs,
+  AllResponseReviewByResponser,
+  AllResponseReviewByReceiver,
+} from './components'
+import { PATH } from './constants'
 
 const CreatedReviewManagePage = () => {
-  const { mutate: sendReview } = useSendReview()
+  const { pathname } = useLocation()
+  const reviewId = pathname.replace(`${PATH.REVIEW_MANAGEMENT}`, '')
 
-  const handleButtonClick = () => {
-    sendReview(
-      { surveyId: 1, subjectIdList: [1, 2, 3] },
-      {
-        onSuccess: ({ data }) => {
-          console.log(data)
-        },
-      },
-    )
+  const { allResponseByReceiver, allResponseByResponser } = useGetAllResponse({
+    reviewId,
+  })
+
+  const [activeTab, setActiveTab] = useState<'responser' | 'receiver'>(
+    'responser',
+  )
+
+  const REVIEW_MANAGE_TAB_CONTENT = {
+    responser: (
+      <AllResponseReviewByResponser
+        responsers={allResponseByResponser.data.responsers}
+        responserCount={allResponseByResponser.data.responserCount}
+      />
+    ),
+    receiver: (
+      <AllResponseReviewByReceiver
+        recipientList={allResponseByReceiver.data.recipientList}
+      />
+    ),
   }
 
   return (
-    <>
-      <h1>생성한 리뷰 관리 페이지</h1>
-      <button className="btn" onClick={handleButtonClick} />
-    </>
+    <h1>
+      <div className="flex justify-center text-4xl">
+        {allResponseByResponser.data.title}
+      </div>
+
+      <ReviewManageTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      <div>{REVIEW_MANAGE_TAB_CONTENT[activeTab]}</div>
+    </h1>
   )
 }
 export default CreatedReviewManagePage
