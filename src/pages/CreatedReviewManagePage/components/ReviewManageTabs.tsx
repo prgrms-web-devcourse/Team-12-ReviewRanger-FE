798e8ea04808a39906cf38d4459a66ee78427749
Kim0426 <706shin1728@naver.com>
2023-11-07 17:00:37 +0900

    feat: 생성한 리뷰 단일 페이지 상수 추가, 탭 컴포넌트 추가

---
+++ src/pages/CreatedReviewManagePage/components/ReviewManageTabs.tsx
@@ -0,0 +1,28 @@
+import {
+  REVIEW_MANAGE_TAB_MENU_STYLE,
+  REVIEW_MANAGE_TAB_TITLE,
+} from '@/pages/CreatedReviewManagePage/constants'
+
+interface TabsProps {
+  activeTab: 'responser' | 'receiver'
+  setActiveTab: React.Dispatch<React.SetStateAction<'responser' | 'receiver'>>
+}
+
+const ReviewManageTabs = ({ activeTab, setActiveTab }: TabsProps) => {
+  const tabBorder = `after:absolute after:bottom-0 after:h-1 after:w-full after:bg-white after-duration-[400ms] after:content-[''] after:transition-transform after:scale-x-[0.25] after:rounded-full ${REVIEW_MANAGE_TAB_MENU_STYLE[activeTab]}`
+
+  return (
+    <div
+      className={`relative flex h-11 bg-main-red-300 text-lg text-white md:h-[3.125rem] md:text-2xl ${tabBorder} sticky top-0 shadow-md`}
+    >
+      <button className="grow" onClick={() => setActiveTab('responser')}>
+        {REVIEW_MANAGE_TAB_TITLE.responser}
+      </button>
+      <button className="grow" onClick={() => setActiveTab('receiver')}>
+        {REVIEW_MANAGE_TAB_TITLE.receiver}
+      </button>
+    </div>
+  )
+}
+
+export default ReviewManageTabs
