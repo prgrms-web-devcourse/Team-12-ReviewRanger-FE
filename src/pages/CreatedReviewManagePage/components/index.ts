e907e3ca4660de213132e401e8eb925ca394be3e
Kim0426 <706shin1728@naver.com>
2023-11-07 18:39:29 +0900

    refactor: index 파일 수정

--- src/pages/CreatedReviewManagePage/components/index.ts
+++ src/pages/CreatedReviewManagePage/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Tabs } from './Tabs'
-export { default as ReviewSelectedUser } from './ReviewSelectedUser'
 export { default as AllResponseReviewByResponser } from './AllResponseReviewByResponser'
 export { default as AllResponseReviewByReceiver } from './AllResponseReviewByReceiver'
