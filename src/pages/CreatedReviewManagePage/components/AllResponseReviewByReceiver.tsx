b71d6c4f56ab29647cff31dc0417b71c8711f06b
Kim0426 <706shin1728@naver.com>
2023-11-08 12:05:46 +0900

    feat: SearchBar 컴포넌트로 이름 검색하게 설정

--- src/pages/CreatedReviewManagePage/components/AllResponseReviewByReceiver.tsx
+++ src/pages/CreatedReviewManagePage/components/AllResponseReviewByReceiver.tsx
@@ -1,11 +1,28 @@
 //NOTE - 수신자 탭
-import { UserList } from '@/components'
+import { useState } from 'react'
+import { UserList, SearchBar } from '@/components'
+
 import { AllReceiverResponse } from '@/apis/hooks/useGetAllRespnse'
 
 const AllResponseReviewByResponser = ({
   recipientList,
 }: AllReceiverResponse) => {
-  return <UserList users={recipientList ?? []} />
+  const [keyword, setKeyword] = useState('')
+
+  const handleChangeKeyword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value)
+  }
+
+  const filteredUsers = recipientList.filter((user) => {
+    return user.recipientName.includes(keyword)
+  })
+
+  return (
+    <>
+      <SearchBar handleChangeKeyword={handleChangeKeyword} />
+      <UserList users={filteredUsers} />
+    </>
+  )
 }
 
 export default AllResponseReviewByResponser
