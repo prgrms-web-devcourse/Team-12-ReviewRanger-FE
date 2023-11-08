efddfbcf80b85bf0d9f6fd3ea3252d9661392a69
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-08 17:46:47 +0900

    refactor: 이름 변경, 비번 변경 msw 변경

--- src/mocks/handlers/profile.ts
+++ src/mocks/handlers/profile.ts
@@ -1,9 +1,15 @@
 import { rest } from 'msw'
 
 export const profileHandlers = [
-  rest.put('/members/profile', async (req, res, ctx) => {
+  rest.patch('/members/profile-name', async (req, res, ctx) => {
     const { name } = await req.json()
     console.log('이름이 ', name, '으로 변경되었습니다.')
+
+    return res(ctx.status(200), ctx.json({ success: true }))
+  }),
+
+  rest.patch('/members/profile-password', async (_, res, ctx) => {
+    console.log('비밀번호가 변경되었습니다.')
 
     return res(ctx.status(200), ctx.json({ success: true }))
   }),
