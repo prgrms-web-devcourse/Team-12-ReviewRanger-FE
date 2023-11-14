678d011e69adcb027bb4b697c6ff205612729b05
Kim0426 <706shin1728@naver.com>
2023-11-15 02:12:15 +0900

    refactor: 작성자별 검색결과 없을 때 다른 컴포넌트 처리

--- src/pages/CreatedReviewManagePage/components/AllResponseReviewByResponser.tsx
+++ src/pages/CreatedReviewManagePage/components/AllResponseReviewByResponser.tsx
@@ -1,7 +1,7 @@
-//NOTE - 작성자별 탭
 import { useState } from 'react'
 import { UserList, SearchBar } from '@/components'
 import { useGetAllResponseByResponser } from '@/apis/hooks'
+import { NoSearchResultIcon } from '@/assets/icons'
 import { SortDropDown } from '../components'
 
 const AllResponseReviewByResponser = ({ surveyId }: { surveyId: string }) => {
@@ -18,22 +18,22 @@ const AllResponseReviewByResponser = ({ surveyId }: { surveyId: string }) => {
   }
 
   const sortByName = () => {
-    setFilteredUsers(() =>
-      [...filteredUsers].sort((a, b) => a.user.name.localeCompare(b.user.name)),
+    setFilteredUsers((prevUsers) =>
+      [...prevUsers].sort((a, b) => a.user.name.localeCompare(b.user.name)),
     )
   }
 
   const sortByResponse = () => {
-    setFilteredUsers(() =>
-      [...filteredUsers].sort(
+    setFilteredUsers((prevUsers) =>
+      [...prevUsers].sort(
         (a, b) => Number(b.isAnswered) - Number(a.isAnswered),
       ),
     )
   }
 
   const sortByNoResponse = () => {
-    setFilteredUsers(() =>
-      [...filteredUsers].sort(
+    setFilteredUsers((prevUsers) =>
+      [...prevUsers].sort(
         (a, b) => Number(a.isAnswered) - Number(b.isAnswered),
       ),
     )
@@ -43,34 +43,44 @@ const AllResponseReviewByResponser = ({ surveyId }: { surveyId: string }) => {
     .map((value) => value.user)
     .filter((user) => user.name.trim().includes(keyword))
 
+  const totalResponses = responseByResponser.length
+  const completedResponses = responseByResponser.filter(
+    (value) => value.isAnswered === true,
+  ).length
+
   return (
     <div className="flex flex-col gap-5">
       <SearchBar
         handleChangeKeyword={handleChangeKeyword}
         className="flex w-full"
       />
-      <div className="max-h-[30rem] max-w-[550px] overflow-auto rounded-md border border-gray-200 bg-main-yellow text-black dark:border-gray-700 dark:bg-main-red-200 dark:text-white">
-        <div className="z-5 sticky top-0 flex items-center whitespace-pre-wrap border-b border-gray-200 bg-main-yellow p-3 dark:bg-main-red-200">
-          <span>응답완료: </span>
-          <span className="text-sub-blue dark:text-sub-skyblue">
-            {
-              responseByResponser.filter((value) => value.isAnswered === true)
-                .length
-            }
-            /{responseByResponser.length}
-          </span>
-          <span>명</span>
-          <SortDropDown
-            sortByName={sortByName}
-            sortByNoResponse={sortByNoResponse}
-            sortByResponse={sortByResponse}
-          />
-        </div>
 
-        <UserList
-          users={findUserBySearchKeyword}
-          submitAt={filteredUsers.map((value) => value.submitAt)}
-        />
+      <div className="max-h-[30rem] max-w-[550px] overflow-auto rounded-md border border-gray-200 bg-main-yellow text-black dark:border-gray-700 dark:bg-main-red-200 dark:text-white">
+        {findUserBySearchKeyword.length ? (
+          <>
+            <div className="z-5 sticky top-0 flex items-center whitespace-pre-wrap border-b border-gray-200 bg-main-yellow p-3 dark:bg-main-red-200">
+              <span>응답완료: </span>
+              <span className="text-sub-blue dark:text-sub-skyblue">
+                {completedResponses}/{totalResponses}명
+              </span>
+              <SortDropDown
+                sortByName={sortByName}
+                sortByNoResponse={sortByNoResponse}
+                sortByResponse={sortByResponse}
+              />
+            </div>
+
+            <UserList
+              users={findUserBySearchKeyword}
+              submitAt={filteredUsers.map((value) => value.submitAt)}
+            />
+          </>
+        ) : (
+          <div className="flex h-[9.375rem] flex-col items-center justify-center">
+            <NoSearchResultIcon className="fill-black dark:fill-white" />
+            <p>검색 결과가 없다!</p>
+          </div>
+        )}
       </div>
     </div>
   )
