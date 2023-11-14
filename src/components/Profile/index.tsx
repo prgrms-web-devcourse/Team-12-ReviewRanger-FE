809391b9d4a626bc50f85e5b1ebf09fe901406e9
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-14 18:14:27 +0900

    refactor: Profile 컴포넌트 스타일 수정

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -6,18 +6,16 @@ interface ProfileProps {
 }
 
 const Profile = ({
-  image = <BasicProfileIcon className="h-4 w-4" />,
+  image = <BasicProfileIcon className="visible h-4 w-4" />,
   name,
 }: ProfileProps) => {
   return (
-    <>
-      <div className="flex flex-row items-center gap-2">
-        <div className="flex h-5 w-5 items-center justify-center overflow-hidden rounded-full border">
-          {typeof image === 'string' ? <img src={image} /> : image}
-        </div>
-        <p className="text-sm dark:text-white">{name}</p>
+    <div className="flex flex-row items-center gap-2">
+      <div className="flex h-5 w-5 items-center justify-center overflow-hidden rounded-full border">
+        {typeof image === 'string' ? <img src={image} /> : image}
       </div>
-    </>
+      <p className="text-sm dark:text-white">{name}</p>
+    </div>
   )
 }
 
