ace4dd25910d1f9622c4487f4fb3a3f8861104d6
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-14 16:20:33 +0900

    style: BasicProfileIcon 스타일 변경

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -5,14 +5,17 @@ interface ProfileProps {
   name: string
 }
 
-const Profile = ({ image = <BasicProfileIcon />, name }: ProfileProps) => {
+const Profile = ({
+  image = <BasicProfileIcon className="h-4 w-4" />,
+  name,
+}: ProfileProps) => {
   return (
     <>
       <div className="flex flex-row items-center gap-2">
-        <div className="flex h-[30px] w-[30px]">
+        <div className="flex h-5 w-5 items-center justify-center overflow-hidden rounded-full border">
           {typeof image === 'string' ? <img src={image} /> : image}
         </div>
-        <div>{name}</div>
+        <p className="text-sm dark:text-white">{name}</p>
       </div>
     </>
   )
