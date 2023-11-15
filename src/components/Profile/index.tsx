341ef5c415b7d19853f74aa5412edc07361d73f0
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-15 14:23:38 +0900

    refactor: Profile 컴포넌트 prop으로 className 추가

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -3,18 +3,22 @@ import { BasicProfileIcon } from '@/assets/icons'
 interface ProfileProps {
   image?: string | JSX.Element
   name: string
+  className?: string
 }
 
 const Profile = ({
   image = <BasicProfileIcon className="visible h-4 w-4" />,
   name,
+  className,
 }: ProfileProps) => {
   return (
     <div className="flex flex-row items-center gap-2">
       <div className="flex h-5 w-5 items-center justify-center overflow-hidden rounded-full border">
         {typeof image === 'string' ? <img src={image} /> : image}
       </div>
-      <p className="text-sm dark:text-white md:text-lg">{name}</p>
+      <p className={`${className} text-sm dark:text-white md:text-lg`}>
+        {name}
+      </p>
     </div>
   )
 }
