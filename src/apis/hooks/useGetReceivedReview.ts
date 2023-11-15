0283348703e2afe9c6eb297b34b16b56431607d1
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-15 23:33:20 +0900

    refactor: 리뷰 결과 조회 api 훅 수정

--- src/apis/hooks/useGetReceivedReview.ts
+++ src/apis/hooks/useGetReceivedReview.ts
@@ -1,29 +1,75 @@
-import { useQuery } from '@tanstack/react-query'
+import { useSuspenseQueries } from '@tanstack/react-query'
 import apiClient from '../apiClient'
 
-interface Result {
+interface FinalResult {
+  userId: number
+  userName: string
+  reviewId: number
+  title: string
+  description: string
+  status: 'SENT' | 'NOT_SENT'
+  createdAt: string
+  updatedAt: string
+}
+
+interface FinalResultQna {
+  questionId: number
   questionTitle: string
-  questionType: string
+  answerIdList: number[]
+}
+
+interface TextResult extends FinalResultQna {
+  finalQuestionType:
+    | 'SINGLE_CHOICE'
+    | 'MULTIPLE_CHOICE'
+    | 'SUBJECTIVE'
+    | 'DROPDOWN'
   answers: string[]
 }
 
+interface RatingResult extends FinalResultQna {
+  finalQuestionType: 'RATING'
+  answers: number[]
+}
+
+interface StatResult extends FinalResultQna {
+  finalQuestionType: 'HEXASTAT'
+  answers: {
+    statName: string
+    statScore: number
+  }[]
+}
+
 interface Response {
-  title: string
-  name: string
-  createdAt: string
-  results: Result[]
+  success: boolean
+  data: FinalResult
+}
+
+interface ResponseQna {
+  success: boolean
+  data: TextResult[] | RatingResult[] | StatResult[]
 }
 
 const useGetReceivedReview = (reviewId: number) => {
   const getReviewResult = async () => {
     const response = await apiClient.get<Response>(`/final-results/${reviewId}`)
 
-    return response.data
+    return response.data.data
   }
 
-  return useQuery({
-    queryKey: ['/final-results/${reviewId}'],
-    queryFn: getReviewResult,
+  const getReviewQna = async () => {
+    const response = await apiClient.get<ResponseQna>(
+      `/final-results/${reviewId}/qna`,
+    )
+
+    return response.data.data
+  }
+
+  return useSuspenseQueries({
+    queries: [
+      { queryKey: ['/final-results/${reviewId}'], queryFn: getReviewResult },
+      { queryKey: ['/final-results/${reviewId}/qna'], queryFn: getReviewQna },
+    ],
   })
 }
 
