6cef0b5e348c820114e228ae7642ca4cd0101965
Kim0426 <706shin1728@naver.com>
2023-11-16 16:34:15 +0900

    refactor: useReviewTab 훅 제거

--- src/pages/CreatedReviewManagePage/index.tsx
+++ src/pages/CreatedReviewManagePage/index.tsx
@@ -1,24 +1,26 @@
 //생성한 리뷰 관리 페이지
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Header } from '@/components'
 import { useGetReviewQuestion } from '@/apis/hooks'
-import useActiveTab from '@/pages/CreatedReviewManagePage/hooks/useReviewTab'
+
 import {
   Tabs,
   AllResponseReviewByResponser,
   AllResponseReviewByReceiver,
 } from './components'
-import { PATH } from './constants'
 
 const CreatedReviewManagePage = () => {
   const { pathname } = useLocation()
   const reviewId = pathname.split('/').at(-1) ?? ''
 
+  const [activeTab, setActiveTab] = useState<'responser' | 'receiver'>(
+    'responser',
+  )
+
   const { data: getReviewQuestion } = useGetReviewQuestion({
     id: reviewId,
   }).data
-  const { activeTab, changeTab } = useActiveTab('responser')
 
   const REVIEW_MANAGE_TAB_CONTENT = {
     responser: (
@@ -49,7 +51,7 @@ const CreatedReviewManagePage = () => {
     <div className="flex h-auto min-h-screen flex-col bg-main-ivory text-black dark:bg-main-red-100 dark:text-white">
       <div className="sticky top-0 z-10">
         <Header />
-        <Tabs activeTab={activeTab} setActiveTab={changeTab} />
+        <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
       </div>
       <div className="mx-auto flex w-full max-w-[800px] flex-col px-5 py-7 md:p-10">
         <h1 className="text-xl md:text-2xl">{getReviewQuestion.title}</h1>
