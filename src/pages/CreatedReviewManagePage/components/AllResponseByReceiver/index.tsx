6d590578278bc849a7edd17f59a3459bb199a5e0
Kim0426 <706shin1728@naver.com>
2023-11-17 04:13:51 +0900

    refactor: 수신자별에서도 상세 갔을 때 아코디안 UI 그리게 수정

--- src/pages/CreatedReviewManagePage/components/AllResponseByReceiver/index.tsx
+++ src/pages/CreatedReviewManagePage/components/AllResponseByReceiver/index.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { useState, Suspense } from 'react'
 import { UserList, SearchBar } from '@/components'
 import { useGetAllResponseByReceiver } from '@/apis/hooks'
-import { NotFoundSearchUser } from '../../components'
+import { NotFoundSearchUser, ReviewDetailAccordion } from '../../components'
 
 const AllResponseReviewByResponser = ({ surveyId }: { surveyId: string }) => {
   const [keyword, setKeyword] = useState('')
 
   const { data: responseByReceiver } = useGetAllResponseByReceiver({
     surveyId,
+  })
+  const [selectedUser, setSelectedUser] = useState<{
+    id: string
+    name: string
+  }>({
+    id: 'user',
+    name: 'user',
   })
 
   const [filteredUsers] = useState(
@@ -42,12 +49,25 @@ const AllResponseReviewByResponser = ({ surveyId }: { surveyId: string }) => {
 
         {shouldDisplayUserList ? (
           <div className="max-h-80 overflow-auto">
+            <input
+              type="checkbox"
+              className="drawer-toggle"
+              id="drawer-bottom"
+            />
             <UserList
               users={findUserBySearchKeyword}
+              onClickUser={({ id, name }) => setSelectedUser({ id, name })}
               responserCount={filteredUsers.map(
                 (value) => value.responserCount,
               )}
             />
+            <Suspense fallback={<div>홀호</div>}>
+              <ReviewDetailAccordion
+                reviewId={surveyId}
+                receiverId={selectedUser.id}
+                receiverName={selectedUser.name}
+              />
+            </Suspense>
           </div>
         ) : (
           <NotFoundSearchUser />
