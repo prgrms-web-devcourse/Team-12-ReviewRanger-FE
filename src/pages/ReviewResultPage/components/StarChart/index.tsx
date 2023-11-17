624f497cdf6f446b9ca7fbfb51b523e4106e6d78
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-17 09:19:06 +0900

    feat: 별점 소수점 비율 구현

--- src/pages/ReviewResultPage/components/StarChart/index.tsx
+++ src/pages/ReviewResultPage/components/StarChart/index.tsx
@@ -1,21 +1,62 @@
-import { StarIcon } from '@/assets/icons'
+import { STAR } from './constant'
 
 interface StarChartProps {
   answer: number[]
 }
 
 const StarChart = ({ answer }: StarChartProps) => {
-  // TODO: 별점 소수점 처리
-  // const average = answer.reduce((acc, cur) => acc + cur, 0) / answer.length
-  console.log(answer)
+  const { ID_LIST, PATH_DATA, SIZE } = STAR
+
+  const calculateFillWidthList = (average: number): number[] => {
+    let totalScore = average
+
+    return Array.from({ length: 5 }, () => {
+      if (totalScore >= 1) {
+        totalScore -= 1
+
+        return SIZE
+      }
+      if (totalScore > 0) {
+        const point = totalScore
+        totalScore = 0
+
+        return SIZE * point
+      }
+
+      return 0
+    })
+  }
+
+  const average = answer.reduce((acc, cur) => acc + cur, 0) / answer.length
+  const fillWidthList = calculateFillWidthList(average)
 
   return (
-    <div className="mx-auto flex gap-4">
-      <StarIcon className="fill-sub-yellow stroke-gray-200 dark:stroke-gray-400" />
-      <StarIcon className="fill-sub-yellow stroke-gray-200 dark:stroke-gray-400" />
-      <StarIcon className="fill-sub-yellow stroke-gray-200 dark:stroke-gray-400" />
-      <StarIcon className="fill-sub-yellow stroke-gray-200 dark:stroke-gray-400" />
-      <StarIcon className="fill-gray-400 stroke-gray-200 dark:stroke-gray-400" />
+    <div className="mx-auto flex items-center gap-2 md:gap-4">
+      {ID_LIST.map((id, index) => {
+        return (
+          <svg
+            key={id}
+            width={SIZE}
+            height={SIZE}
+            viewBox={`0 0 ${SIZE} ${SIZE}`}
+            xmlns="http://www.w3.org/2000/svg"
+            className="transform fill-gray-400 stroke-gray-200 transition-transform duration-300 ease-in-out hover:scale-110 dark:stroke-gray-400"
+          >
+            <clipPath id={`${id}Clip`}>
+              <rect width={fillWidthList[index]} height={SIZE} />
+            </clipPath>
+            <path id={id} d={PATH_DATA} transform="translate(3 0)" />
+            <use
+              clipPath={`url(#${id}Clip)`}
+              href={`#${id}`}
+              className="fill-sub-yellow"
+            />
+          </svg>
+        )
+      })}
+      <p className="text-sm text-gray-200 md:text-lg">
+        {average.toFixed(2)} 점
+      </p>
     </div>
   )
 }
