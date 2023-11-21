d6b6eb27aa8c9cba72462054e957d74a3e1a5b93
Kim0426 <706shin1728@naver.com>
2023-11-21 14:57:28 +0900

    refactor: 수신자별 상세 탭에서 미리보기 버튼 제거

--- src/pages/CreatedReviewManagePage/components/ReviewDetailAccordion/index.tsx
+++ src/pages/CreatedReviewManagePage/components/ReviewDetailAccordion/index.tsx
@@ -95,11 +95,6 @@ const ReviewDetailAccordion = ({
               )}
             />
           ))}
-          <div className="flex h-auto justify-end gap-2">
-            <button className="btn h-[2.5rem] w-[6.25rem] rounded-md bg-active-orange p-0 text-lg text-white dark:text-black">
-              미리 보기
-            </button>
-          </div>
         </div>
       </div>
     </>
