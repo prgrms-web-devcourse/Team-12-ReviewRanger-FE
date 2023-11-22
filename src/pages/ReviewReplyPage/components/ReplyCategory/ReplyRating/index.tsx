002afb9d1030e165291caf0800a0e0e6aabec6b2
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-23 04:53:44 +0900

    fix: 별점 답변 에러 수정하기

--- src/pages/ReviewReplyPage/components/ReplyCategory/ReplyRating/index.tsx
+++ src/pages/ReviewReplyPage/components/ReplyCategory/ReplyRating/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { useStarRate } from '@/hooks/useStarRate/useStarRate'
 import { ReviewReplyType } from '@/pages/ReviewReplyPage/types'
@@ -18,29 +18,41 @@ const ReplyRating = ({
   handleCheckReply,
 }: ReplyRatingProps) => {
   const registerPath: RegisterPath = `replyTargets.${receiverIndex}.replies.${questionIndex}`
-  const { changeStar, rates, setRates, score } = useStarRate()
+  const { changeStar, rates, setRates } = useStarRate()
   const { setValue, getValues } = useFormContext<ReviewReplyType>()
 
+  const prevScore = useMemo(
+    () => getValues(`${registerPath}.answerRating`),
+    [registerPath, getValues],
+  )
+
+  const [score, setScore] = useState(prevScore || 0)
+
   useEffect(() => {
-    const prevScore = getValues(`${registerPath}.answerRating`)
     setRates(
       prevScore
         ? Array.from({ length: 5 }, (_, index) => index < prevScore)
         : Array(5).fill(false),
     )
-  }, [registerPath, getValues, setRates])
+    setScore(prevScore || 0)
+  }, [prevScore, receiverIndex, setRates])
 
   useEffect(() => {
-    score && setValue(`${registerPath}.answerRating`, score)
+    setValue(`${registerPath}.answerRating`, score)
   }, [score, registerPath, setValue])
 
   useEffect(() => {
     handleCheckReply({ value: score })
   }, [score, handleCheckReply])
 
+  const handleClickStar = (index: number) => {
+    setScore(index + 1)
+    changeStar(index)
+  }
+
   return (
     <div className="flex justify-center">
-      <StarRatingList handleClickStar={changeStar} rates={rates} />
+      <StarRatingList handleClickStar={handleClickStar} rates={rates} />
     </div>
   )
 }
