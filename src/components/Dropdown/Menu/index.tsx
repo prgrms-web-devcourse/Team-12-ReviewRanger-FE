970b474cebc235c907f4dd92dd6ebaad19033d0c
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-22 16:53:35 +0900

    style: 드롭다운 center 추가

--- src/components/Dropdown/Menu/index.tsx
+++ src/components/Dropdown/Menu/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 interface MenuProps {
   className?: string
-  position?: 'right' | 'left'
+  position?: 'right' | 'left' | 'center'
 }
 
 const Menu = ({
@@ -10,11 +10,15 @@ const Menu = ({
   position = 'left',
   children,
 }: PropsWithChildren<MenuProps>) => {
-  const menuPosition = `dropdown-menu-bottom-${position}`
+  const menuPosition = {
+    right: 'dropdown-menu-bottom-right',
+    left: 'dropdown-menu-bottom-left',
+    center: 'dropdown-menu-bottom-center',
+  }
 
   return (
     <div
-      className={`dropdown-menu rounded-md border border-black bg-white shadow-md dark:border-white dark:bg-main-gray ${menuPosition} ${className}`}
+      className={`dropdown-menu rounded-md border border-black bg-white shadow-md dark:border-white dark:bg-main-gray ${menuPosition[position]} ${className}`}
     >
       {children}
     </div>
