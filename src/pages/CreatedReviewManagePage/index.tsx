ff3ec6168ac07be252fd6ad6db8bcccbee3afaec
Kim0426 <706shin1728@naver.com>
2023-11-22 17:33:48 +0900

    feat: 모든 수신자가 결과를 받았는지 검증하는 훅 추가

--- src/pages/CreatedReviewManagePage/index.tsx
+++ src/pages/CreatedReviewManagePage/index.tsx
@@ -2,7 +2,11 @@
 import { Suspense, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Header } from '@/components'
-import { useCloseSurvey, useGetReviewQuestion } from '@/apis/hooks'
+import {
+  useCloseSurvey,
+  useGetReviewQuestion,
+  useCheckAllReceiverReceived,
+} from '@/apis/hooks'
 
 import {
   Tabs,
@@ -22,6 +26,11 @@ const CreatedReviewManagePage = () => {
 
   //NOTE - 리뷰의 질문을 가져온다!
   const { data: getReviewQuestion } = useGetReviewQuestion({
+    id: reviewId,
+  }).data
+
+  //NOTE - 마감 이후 호출 되어야 함
+  const { data: checkAllRecipientReceived } = useCheckAllReceiverReceived({
     id: reviewId,
   }).data
 
