f5528a1d4e3a8fe34dfa567e8150f2a1267abb98
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-24 05:40:26 +0900

    refactor: useGetResponseByResponser api 수정

--- src/apis/hooks/useGetResponseByResponser.ts
+++ src/apis/hooks/useGetResponseByResponser.ts
@@ -1,20 +1,56 @@
 //NOTE - 작성자별 응답 결과 단일 조회
 import { useSuspenseQuery } from '@tanstack/react-query'
 import apiClient from '@/apis/apiClient'
-import { Response } from './useGetResponseByReceiver'
 
-//NOTE - 참여 ID
-const useGetResponseByResponser = ({ id }: { id: string }) => {
+interface AnswerChoice {
+  optionId: number
+  optionName: string
+}
+
+interface Reply {
+  id: number
+  questionId: number
+  answerText: string | null
+  answerRating: number | null
+  answerHexa: number | null
+  answerChoice: AnswerChoice | null
+}
+
+interface User {
+  id: number
+  email: string
+  name: string
+}
+interface Data {
+  id: number
+  participationId: number
+  replies: Reply[]
+  receiver: User
+  responser: User
+}
+
+interface Response {
+  success: boolean
+  data: Data[]
+}
+
+const useGetResponseByResponser = ({
+  reviewId,
+  responserId,
+}: {
+  reviewId: number
+  responserId: number
+}) => {
   const getSingleAuthorResponse = async () => {
     const singleAuthorResponse = await apiClient.get<Response>(
-      `/reviewed-targets/${id}/responser`,
+      `/reviews/${reviewId}/responser/${responserId}`,
     )
 
-    return singleAuthorResponse.data
+    return singleAuthorResponse.data.data
   }
 
   return useSuspenseQuery({
-    queryKey: [`/reviewed-targets/${id}/responser`],
+    queryKey: [`/reviews/${reviewId}/responser/${responserId}`],
     queryFn: getSingleAuthorResponse,
   })
 }
