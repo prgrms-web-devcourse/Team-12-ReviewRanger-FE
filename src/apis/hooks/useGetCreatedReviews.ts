730d357c582835c4dfff1977703a1997da18ec81
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-23 21:19:51 +0900

    feat: 리뷰 리스트 infiniteQuery로 구현

---
+++ src/apis/hooks/useGetCreatedReviews.ts
@@ -0,0 +1,48 @@
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
+import { CreatedReview } from '@/types'
+import apiClient from '../apiClient'
+
+interface CreatedReviewResponse {
+  success: boolean
+  data: {
+    content: CreatedReview[]
+    last: boolean
+  }
+}
+
+interface PageParam {
+  pageParam: number | null
+}
+
+const useGetCreatedReviews = () => {
+  const getCreatedReview = async ({ pageParam }: PageParam) => {
+    const response = await apiClient.get<CreatedReviewResponse>('/reviews', {
+      params: { cursorId: pageParam, size: 12 },
+    })
+
+    return response.data.data
+  }
+
+  return useSuspenseInfiniteQuery({
+    queryKey: ['/reviews'],
+    queryFn: ({ pageParam }) => getCreatedReview({ pageParam }),
+    initialPageParam: null as number | null,
+    getNextPageParam: (lastPage) => {
+      const { last, content } = lastPage
+
+      if (last || content.length === 0) {
+        return undefined
+      }
+
+      console.log(content[content.length - 1].reviewId)
+
+      return content[content.length - 1].reviewId
+    },
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchInterval: 5000,
+    refetchIntervalInBackground: false,
+  })
+}
+
+export default useGetCreatedReviews
