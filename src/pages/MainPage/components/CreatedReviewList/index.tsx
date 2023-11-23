d5dde672c2e75aa7c00115210e57045b05ceb5a7
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-23 21:48:43 +0900

    feat: 리뷰 생성 버튼 추가

--- src/pages/MainPage/components/CreatedReviewList/index.tsx
+++ src/pages/MainPage/components/CreatedReviewList/index.tsx
@@ -2,15 +2,20 @@ import { nanoid } from 'nanoid'
 import { useEffect } from 'react'
 import { useInfiniteScroll } from '@/hooks'
 import { useGetCreatedReviews } from '@/apis/hooks'
+import { PlusIcon } from '@/assets/icons'
 import { scrollToTop } from '@/utils'
 import { CreatedReview } from '@/types'
 import CreatedReviewItem from './CreatedReviewItem'
 
 interface CreatedReviewListProps {
-  handleReviewClick: (id: number) => void
+  handleClickReview: (id: number) => void
+  handleClickAddReview: () => void
 }
 
-const CreatedReviewList = ({ handleReviewClick }: CreatedReviewListProps) => {
+const CreatedReviewList = ({
+  handleClickReview,
+  handleClickAddReview,
+}: CreatedReviewListProps) => {
   const { data, hasNextPage, fetchNextPage } = useGetCreatedReviews()
 
   useEffect(() => {
@@ -31,9 +36,16 @@ const CreatedReviewList = ({ handleReviewClick }: CreatedReviewListProps) => {
 
   return (
     <ul className="grid grid-cols-2 gap-8 sm:grid-cols-3 md:grid-cols-4 md:gap-10">
+      <button
+        className="btn h-36 rounded-md border border-gray-100 bg-main-yellow transition-transform dark:border-white dark:bg-main-red-200 md:h-40"
+        onClick={handleClickAddReview}
+      >
+        <PlusIcon className="h-6 w-6 fill-gray-200 dark:fill-white" />
+      </button>
+
       {reviews.map((review) => (
         <CreatedReviewItem
-          handleReviewClick={handleReviewClick}
+          handleClickReview={handleClickReview}
           key={nanoid()}
           className="btn flex h-36 flex-col items-stretch justify-between rounded-md border border-gray-100 bg-main-yellow p-2.5 transition-transform dark:border-white dark:bg-main-red-200 md:h-40"
           {...review}
