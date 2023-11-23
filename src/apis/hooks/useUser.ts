f65b46dd10e87b27666d53ea641f323760c72f16
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-23 09:54:18 +0900

    feat: enabled 속성 추가

--- src/apis/hooks/useUser.ts
+++ src/apis/hooks/useUser.ts
@@ -1,5 +1,6 @@
-import { useSuspenseQuery } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import apiClient from '@/apis/apiClient'
+import { TOKEN_KEY } from '@/constants'
 
 export interface User {
   success: boolean
@@ -14,16 +15,17 @@ const useUser = () => {
   const getUser = async () => {
     const user = await apiClient.get<User>('/user')
 
-    return user.data
+    return user.data.data
   }
 
-  return useSuspenseQuery({
+  return useQuery({
     queryKey: ['/user'],
     queryFn: getUser,
     refetchOnMount: false,
     refetchOnReconnect: false,
     refetchOnWindowFocus: false,
     staleTime: Infinity,
+    enabled: !!localStorage.getItem(TOKEN_KEY),
   })
 }
 
