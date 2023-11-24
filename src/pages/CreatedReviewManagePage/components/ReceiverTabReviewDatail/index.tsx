9fd411b4dde68bce90fa22d4ba412ed909c47b41
Kim0426 <706shin1728@naver.com>
2023-11-24 15:39:32 +0900

    refactor: 작성자별 상세 컴포넌트 추가

--- src/pages/CreatedReviewManagePage/components/ReceiverTabReviewDatail/index.tsx
+++ src/pages/CreatedReviewManagePage/components/ReceiverTabReviewDatail/index.tsx
@@ -5,7 +5,7 @@ import {
   useSaveFinalResult,
 } from '@/apis/hooks'
 import { CloseDropDownIcon } from '@/assets/icons'
-import { ProfileGroup, QuestionGroup } from '..'
+import { ProfileGroup, ReceiverQuestionGroup } from '..'
 import { getAnswer } from '../../utils'
 
 interface ReviewDetailAccordionProps {
@@ -85,7 +85,7 @@ const ReceiverReviewDetail = ({
             responserSize={responserCount?.size}
           />
           {getReviewQuestion?.questions?.map((question) => (
-            <QuestionGroup
+            <ReceiverQuestionGroup
               questionType={question?.type}
               questionTitle={question?.title}
               key={question?.id}
