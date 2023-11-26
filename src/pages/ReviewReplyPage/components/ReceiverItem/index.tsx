cd170a06cca32ed1748d9cc00d34b2cc7416fd5d
hayamaster <dnfxmfkdbzos@naver.com>
2023-11-27 03:12:43 +0900

    refactor: ReceiverItem 컴포넌트 분리

---
+++ src/pages/ReviewReplyPage/components/ReceiverItem/index.tsx
@@ -0,0 +1,57 @@
+import { MouseEvent } from 'react'
+import { Profile } from '@/components'
+import { Receiver } from '@/apis/hooks/useGetReviewFirst'
+import { CheckInTheCircleIcon } from '@/assets/icons'
+
+interface ReceiverItemProps {
+  receiver: Receiver
+  index: number
+  selectedReceiver: Receiver
+  individualReplyCompletes: boolean[]
+  handleClickReceiver: (e: MouseEvent<HTMLLIElement>) => void
+  checkReplyComplete: () => void
+}
+
+const ReceiverItem = ({
+  receiver,
+  index,
+  selectedReceiver,
+  individualReplyCompletes,
+  handleClickReceiver,
+  checkReplyComplete,
+}: ReceiverItemProps) => {
+  const { receiverId, name } = receiver
+
+  return (
+    <li
+      value={receiverId}
+      onClick={(e) => {
+        handleClickReceiver(e)
+        checkReplyComplete()
+      }}
+      className={`flex h-fit shrink-0 items-center justify-center gap-2 rounded-md border px-2 
+              py-1.5
+              ${
+                selectedReceiver.receiverId === receiverId
+                  ? 'border-black bg-main-yellow dark:border-white dark:bg-main-red-300'
+                  : 'border-gray-100 bg-white dark:border-gray-300 dark:bg-main-red-200'
+              } ${individualReplyCompletes[index] && 'border-sub-green'}`}
+    >
+      <Profile
+        name={name}
+        className={`${
+          selectedReceiver.name === name
+            ? 'text-black dark:text-white'
+            : 'text-gray-300 dark:text-gray-100'
+        }`}
+      />
+      <div className="h-4 w-4">
+        {individualReplyCompletes[index] && (
+          <CheckInTheCircleIcon className="h-4 w-4 fill-sub-green" />
+        )}
+      </div>
+    </li>
+  )
+}
+
+export default ReceiverItem
--- src/pages/ReviewReplyPage/components/index.ts
+++ src/pages/ReviewReplyPage/components/index.ts
@@ -1,3 +1,4 @@
 export { default as ReviewReplyStart } from './ReviewReplyStart'
 export { default as ReviewReplyEdit } from './ReviewReplyEdit'
 export { default as ReviewReplyEnd } from './ReviewReplyEnd'
+export { default as ReceiverItem } from './ReceiverItem'
