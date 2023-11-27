2032fd7837ea54e001856ae0744bf1e6facc651b
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-28 00:35:02 +0900

    feat: 리뷰 삭제 모달, 드롭다운 추가

--- src/pages/MainPage/components/CreatedReviewList/CreatedReviewItem.tsx
+++ src/pages/MainPage/components/CreatedReviewList/CreatedReviewItem.tsx
@@ -1,11 +1,15 @@
 import dayjs from 'dayjs'
+import { Dropdown, Modal } from '@/components'
 import { MenuIcon } from '@/assets/icons'
 import { CreatedReview } from '@/types'
 import { STATUS, STATUS_STYLE } from '../../constants'
 
 interface CreatedReviewItemProps extends CreatedReview {
-  className: string
   handleClickReview: ({ reviewId }: Pick<CreatedReview, 'reviewId'>) => void
+  handleDeleteReview: ({
+    reviewId,
+    status,
+  }: Pick<CreatedReview, 'reviewId' | 'status'>) => void
 }
 
 const CreatedReviewItem = ({
@@ -13,35 +17,51 @@ const CreatedReviewItem = ({
   title,
   status,
   createdAt,
-  className,
   handleClickReview,
+  handleDeleteReview,
 }: CreatedReviewItemProps) => {
   return (
-    <div className={className} onClick={() => handleClickReview({ reviewId })}>
-      <div className="flex items-center justify-between">
-        <div className="flex gap-1.5">
-          <span
-            className={`${STATUS_STYLE[status]} badge rounded-full border-none text-xs font-medium text-white md:text-sm`}
-          >
-            {STATUS[status]}
-          </span>
+    <>
+      <div
+        className="btn flex h-36 flex-col items-stretch justify-between rounded-md border border-gray-100 bg-main-yellow p-2.5 transition-transform dark:border-white dark:bg-main-red-200 md:h-40"
+        onClick={() => handleClickReview({ reviewId })}
+      >
+        <div className="flex items-center justify-between">
+          <div className="flex gap-1.5">
+            <span
+              className={`${STATUS_STYLE[status]} badge rounded-full border-none text-xs font-medium text-white md:text-sm`}
+            >
+              {STATUS[status]}
+            </span>
+          </div>
+
+          <Dropdown>
+            <Dropdown.Toggle className="cursor-pointer">
+              <MenuIcon className="rounded-full stroke-black hover:scale-110 hover:bg-main-hover-yellow dark:stroke-white" />
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="w-28 rounded-md border-gray-100">
+              <label htmlFor={`delete${reviewId}`} className="cursor-pointer">
+                <Dropdown.Item>리뷰 삭제</Dropdown.Item>
+              </label>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
 
-        <MenuIcon
-          className="stroke-black dark:stroke-white"
-          onClick={(e) => {
-            e.stopPropagation()
-          }}
-        />
+        <p className="line-clamp-2 text-center text-base text-black dark:text-sub-red-100 md:text-xl">
+          {title}
+        </p>
+        <p className="text-right text-xs text-black dark:text-sub-red-100 md:text-sm">
+          생성일: {dayjs(createdAt).format('YYYY-MM-DD')}
+        </p>
       </div>
 
-      <p className="line-clamp-2 text-center text-base text-black dark:text-sub-red-100 md:text-xl">
-        {title}
-      </p>
-      <p className="text-right text-xs text-black dark:text-sub-red-100 md:text-sm">
-        생성일: {dayjs(createdAt).format('YYYY-MM-DD')}
-      </p>
-    </div>
+      <Modal
+        modalId={`delete${reviewId}`}
+        content="정말 삭제하시겠습니까?"
+        label="삭제"
+        handleClickLabel={() => handleDeleteReview({ reviewId, status })}
+      />
+    </>
   )
 }
 
