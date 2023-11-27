b51db596379cbe82d03d8f2a1e5d25faef1b86c9
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-28 01:35:46 +0900

    feat: 레이아웃에 에러 바운더리 감싸기

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,25 +1,24 @@
 import { Outlet } from 'react-router-dom'
 import { useDarkMode } from '@/hooks'
-import { QueryErrorBoundary } from '@/components'
+import { ApiErrorBoundary } from '@/components'
 
 const Layout = () => {
   const { toggle } = useDarkMode()
 
   return (
-    <QueryErrorBoundary>
+    <ApiErrorBoundary>
       <div className="bg-white dark:bg-black">
         <div className="border-x-1 mx-auto min-h-screen border-black bg-main-ivory dark:border-white dark:bg-main-red-100">
           <Outlet />
         </div>
       </div>
-
       <button
         className="btn fixed bottom-10 right-20 bg-pink-800 text-white"
         onClick={toggle}
       >
         토글
       </button>
-    </QueryErrorBoundary>
+    </ApiErrorBoundary>
   )
 }
 
