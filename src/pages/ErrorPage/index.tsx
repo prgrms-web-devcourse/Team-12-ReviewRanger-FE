1bb161dcf77e4c045e020b8defb15a813d8e75f2
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-28 02:38:11 +0900

    style: 에러 페이지 배경색 변경

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -2,11 +2,11 @@ import type { FallbackProps } from 'react-error-boundary'
 import { useNavigate } from 'react-router-dom'
 import { errorImg, errorLightImg, sansDark, sansLight } from '@/assets/images'
 
-const ErrorPage = ({ resetErrorBoundary }: FallbackProps) => {
+const ErrorPage = ({ resetErrorBoundary }: Partial<FallbackProps>) => {
   const navigate = useNavigate()
 
   return (
-    <div className="mx-auto flex h-screen flex-col items-center justify-center gap-8 px-5 text-center">
+    <div className="mx-auto flex h-screen flex-col items-center justify-center gap-8 bg-main-ivory px-5 text-center dark:bg-main-red-100">
       <img src={errorImg} alt="에러 메시지" className="mx-auto dark:hidden" />
       <img
         src={errorLightImg}
@@ -24,7 +24,7 @@ const ErrorPage = ({ resetErrorBoundary }: FallbackProps) => {
         className="btn w-full max-w-[15.625rem] rounded-md bg-active-orange text-lg text-white dark:text-black md:text-xl"
         onClick={() => {
           navigate('/')
-          resetErrorBoundary()
+          resetErrorBoundary && resetErrorBoundary()
         }}
       >
         홈으로
