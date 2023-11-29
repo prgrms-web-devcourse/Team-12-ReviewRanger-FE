6e9db6d0aee78c8e577b5912e3e1368ec9ef65bb
Kim0426 <706shin1728@naver.com>
2023-11-29 13:13:46 +0900

    refactor: 드롭다운 스타일 수정

--- src/pages/CreatedReviewManagePage/components/ResponserTabReviewDetail/index.tsx
+++ src/pages/CreatedReviewManagePage/components/ResponserTabReviewDetail/index.tsx
@@ -42,11 +42,11 @@ const ReceiverReviewDetail = ({
   return (
     <>
       <label htmlFor="drawer" className="overlay"></label>
-      <div className="drawer drawer-bottom m-0 flex h-4/5 w-full  flex-col items-center gap-10 overflow-auto bg-main-ivory dark:bg-main-red-100 md:h-[32rem]">
+      <div className="drawer drawer-bottom m-0 flex h-5/6 w-full  flex-col items-center gap-10 overflow-auto bg-main-ivory dark:bg-main-red-100 md:h-5/6">
         <div className="sticky top-0 z-50 flex h-[30px] w-full shrink-0 flex-col items-center justify-center bg-main-yellow dark:bg-main-red-200 sm:h-[40px]">
           <label
             htmlFor="drawer-bottom"
-            className="m-0 flex w-full justify-center"
+            className="flex w-full cursor-pointer justify-center"
           >
             <CloseDropDownIcon className="h-[1rem] w-[1rem] cursor-pointer fill-black stroke-black text-black dark:fill-white dark:stroke-white dark:text-white md:h-[1.25rem] md:w-[1.25rem]" />
           </label>
