846ff60a2e80f5b0cca55ee118727fcbb67358eb
Kim0426 <706shin1728@naver.com>
2023-12-01 04:12:59 +0900

    refactor: 불필요 코드 제거

--- src/pages/CreatedReviewManagePage/components/AnswerGroup/index.tsx
+++ src/pages/CreatedReviewManagePage/components/AnswerGroup/index.tsx
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid'
-import { useCallback, useState } from 'react'
+import { useCallback, useState, memo } from 'react'
 import { BasicProfile } from '@/assets/images'
 import { QUESTION_TYPE } from '../../constants'
 import RenderHexaStat from './RenderHexaStat'
@@ -43,116 +43,98 @@ const renderDefault = (value: Answer) => (
   </>
 )
 
-const QuestionAnswerRenderer = ({
-  answers,
-  questionType,
-  questionTitle,
-  role,
-  questionId,
-  reviewId,
-  userId,
-}: QuestionGroupProps) => {
-  const [inputId] = useState(nanoid())
-  const renderResponseByQuestion = useCallback(
-    (
-      questionId: number,
-      reviewId: string,
-      userId: string,
-      value: Answer,
-      answers: Answer[],
-      questionType: string,
-      index?: number,
-      role?: 'responser' | 'receiver',
-    ) => (
-      <article
-        className="accordion-content w-full border-x border-gray-200 text-black dark:text-white"
-        key={nanoid()}
-      >
-        <div className="accordion-content border-none px-2.5">
-          {(() => {
-            switch (questionType) {
-              case 'RATING':
-                return (
-                  <RenderStarRatingAnswer
-                    userName={value.userName}
-                    value={value.value}
-                  />
-                )
-              case 'HEXASTAT':
-                return <RenderHexaStat value={value} answers={answers} />
-              case 'SUBJECTIVE':
-                return (
-                  <RenderSubjectiveAnswer
-                    text={answers.map(({ value }) => value).join('\n')}
-                    questionId={questionId}
-                    reviewId={Number(reviewId)}
-                    userId={userId}
-                    value={value}
-                    role={role}
-                    isLastAnswer={answers.length - 1 === index}
-                  />
-                )
-              default:
-                return renderDefault(value)
-            }
-          })()}
-        </div>
-      </article>
-    ),
-    [],
-  )
+const QuestionAnswerRenderer = memo(
+  ({
+    answers,
+    questionType,
+    questionTitle,
+    role,
+    questionId,
+    reviewId,
+    userId,
+  }: QuestionGroupProps) => {
+    const [inputId] = useState(nanoid())
+    const renderResponseByQuestion = useCallback(
+      (
+        questionId: number,
+        reviewId: string,
+        userId: string,
+        value: Answer,
+        answers: Answer[],
+        questionType: string,
+        index?: number,
+        role?: 'responser' | 'receiver',
+      ) => (
+        <article
+          className="accordion-content w-full border-x border-gray-200 text-black dark:text-white"
+          key={nanoid()}
+        >
+          <div className="accordion-content border-none px-2.5">
+            {(() => {
+              switch (questionType) {
+                case 'RATING':
+                  return (
+                    <RenderStarRatingAnswer
+                      userName={value.userName}
+                      value={value.value}
+                    />
+                  )
+                case 'HEXASTAT':
+                  return <RenderHexaStat value={value} answers={answers} />
+                case 'SUBJECTIVE':
+                  return (
+                    <RenderSubjectiveAnswer
+                      text={answers.map(({ value }) => value).join('\n')}
+                      questionId={questionId}
+                      reviewId={Number(reviewId)}
+                      userId={userId}
+                      value={value}
+                      role={role}
+                      isLastAnswer={answers.length - 1 === index}
+                    />
+                  )
+                default:
+                  return renderDefault(value)
+              }
+            })()}
+          </div>
+        </article>
+      ),
+      [],
+    )
 
-  return (
-    <div
-      className={`${
-        role === 'responser' ? 'accordion accordion-open' : 'accordion'
-      } bg-white dark:bg-main-gray`}
-    >
-      <input type="checkbox" id={inputId} className="accordion-toggle" />
-      <label
-        htmlFor={inputId}
-        className="accordion-title flex h-10 justify-center border border-gray-200 bg-white px-2.5 text-lg dark:border-gray-400 dark:bg-main-gray"
+    return (
+      <div
+        className={`${
+          role === 'responser' ? 'accordion accordion-open' : 'accordion'
+        } bg-white dark:bg-main-gray`}
       >
-        <div className="flex h-11 items-center text-lg md:text-xl">
-          {QUESTION_TYPE[questionType]}
-          <span className="ml-[0.63rem] text-lg md:text-xl">
-            {questionTitle}
+        <input type="checkbox" id={inputId} className="accordion-toggle" />
+        <label
+          htmlFor={inputId}
+          className="accordion-title flex h-10 justify-center border border-gray-200 bg-white px-2.5 text-lg dark:border-gray-400 dark:bg-main-gray"
+        >
+          <div className="flex h-11 items-center text-lg md:text-xl">
+            {QUESTION_TYPE[questionType]}
+            <span className="ml-[0.63rem] text-lg md:text-xl">
+              {questionTitle}
+            </span>
+          </div>
+
+          <span className="accordion-icon absolute top-1/2 flex -translate-y-1/2 items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path>
+            </svg>
           </span>
-        </div>
+        </label>
 
-        <span className="accordion-icon absolute top-1/2 flex -translate-y-1/2 items-center justify-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path>
-          </svg>
-        </span>
-      </label>
-
-      {questionType !== 'HEXASTAT' &&
-        answers?.map((value, index) =>
-          renderResponseByQuestion(
-            questionId,
-            reviewId,
-            userId,
-            value,
-            answers,
-            questionType,
-            index,
-            role,
-          ),
-        )}
-
-      {questionType === 'HEXASTAT' &&
-        answers
-          ?.filter(
-            (answer, index, self) =>
-              index === self?.findIndex((a) => a.name === answer.name),
-          )
-          ?.map((value) =>
+        {questionType !== 'HEXASTAT' &&
+          answers?.map((value, index) =>
             renderResponseByQuestion(
               questionId,
               reviewId,
@@ -160,10 +142,30 @@ const QuestionAnswerRenderer = ({
               value,
               answers,
               questionType,
+              index,
+              role,
             ),
           )}
-    </div>
-  )
-}
+
+        {questionType === 'HEXASTAT' &&
+          answers
+            ?.filter(
+              (answer, index, self) =>
+                index === self?.findIndex((a) => a.name === answer.name),
+            )
+            ?.map((value) =>
+              renderResponseByQuestion(
+                questionId,
+                reviewId,
+                userId,
+                value,
+                answers,
+                questionType,
+              ),
+            )}
+      </div>
+    )
+  },
+)
 
 export default QuestionAnswerRenderer
