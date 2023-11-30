28ec0f61524ebd3489a5a64bccd29b12773260cb
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-30 14:55:02 +0900

    style: 다크모드 텍스트 색 수정

--- src/pages/MainPage/components/EmptyReview/index.tsx
+++ src/pages/MainPage/components/EmptyReview/index.tsx
@@ -4,7 +4,7 @@ const EmptyReview = ({ message }: { message: string }) => {
   return (
     <div className="mx-auto mt-24 flex w-80 flex-col items-center justify-center gap-4">
       <AlertIcon className="dark:fill-white md:h-8 md:w-8" />
-      <p className="text-base md:text-xl">{message}</p>
+      <p className="text-base dark:text-white md:text-xl">{message}</p>
     </div>
   )
 }
