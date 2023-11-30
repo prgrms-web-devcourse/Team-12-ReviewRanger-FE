f4b9574d1200e31bf4ea4fdb3ed8c3768dc48a76
hayamaster <dnfxmfkdbzos@naver.com>
2023-12-01 04:25:01 +0900

    refactor: 수신자 선택 모달 문구 변경

--- src/pages/ReviewReplyPage/components/ReviewReplyStart/ReceiverSelect.tsx
+++ src/pages/ReviewReplyPage/components/ReviewReplyStart/ReceiverSelect.tsx
@@ -203,7 +203,7 @@ const ReceiverSelect = ({ setReviewStep, questions }: ReceiverSelectProps) => {
       <label htmlFor="select-receiver" ref={modalLabelRef}></label>
       <Modal
         modalId="select-receiver"
-        content={`선택하신 수신자는 이후 변경할 수 없습니다.\n\n모든 수신자를 고르셨습니까?`}
+        content={`선택하신 수신자는 이후 변경할 수 없습니다.\n\n리뷰 대상자를 총 ${receivers.length}명 고르셨습니다.`}
         label="확인"
         handleClickLabel={completeReceiverSelect}
       />
