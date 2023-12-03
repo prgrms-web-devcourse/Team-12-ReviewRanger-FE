3acc475a5676fa0a749921035ef19583addb7815
Kim0426 <706shin1728@naver.com>
2023-12-04 03:02:47 +0900

    feat: api key 은닉 (#106)
    * feat: vite 프록시 설정
    
    * feat: apiClient baseURL주소 수정
    
    * refactor: manualChunks 옵션 제거

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { TOKEN_KEY } from '@/constants'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_KEY,
+  baseURL: '/api',
 })
 
 apiClient.interceptors.request.use(
