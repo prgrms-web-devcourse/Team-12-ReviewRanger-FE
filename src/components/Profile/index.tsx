159228c28cecdf7464fdb16715d95dce3c28a11f
hayamaster <dnfxmfkdbzos@naver.com>
2023-12-04 17:33:41 +0900

    refactor: Profile 컴포넌트 리펙토링

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,17 +1,18 @@
 import { BasicProfile } from '@/assets/images'
 
 interface ProfileProps {
-  image?: string | JSX.Element
+  image?: string
   name: string
   className?: string
 }
 
-const Profile = ({ image = BasicProfile, name, className }: ProfileProps) => {
+const Profile = ({ image, name, className }: ProfileProps) => {
   return (
     <div className="flex flex-row items-center gap-2">
-      <div className="flex h-5 w-5 items-center justify-center overflow-hidden rounded-full border bg-white dark:bg-black">
-        {typeof image === 'string' ? <img src={image} /> : image}
-      </div>
+      <img
+        src={image || BasicProfile}
+        className="flex h-5 w-5 items-center justify-center overflow-hidden rounded-full border bg-white dark:bg-black"
+      />
       <p className={`${className} text-sm dark:text-white md:text-lg`}>
         {name}
       </p>
